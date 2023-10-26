a42943cc8f5e9f62b3ed2a03d1fcf098c633636d
yogskr <96821189+yogskr@users.noreply.github.com>
2023-10-26 14:56:22 +0700

    added a function

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,36 @@
 "use strict";
 
 // navbar fixed
+const header = document.querySelector("header");
+const fixedNav = header.offsetTop;
 
 window.onscroll = () => {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
-
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
   } else {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// scroll up button
+const scrollBtn = document.querySelector("#scroll-button");
+const bodyElement = document.body;
+const docElement = document.documentElement;
+const showOnPx = 50;
+
+const scrollContainer = () => {
+  return docElement || bodyElement;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    scrollBtn.classList.remove("hidden");
+  } else {
+    scrollBtn.classList.add("hidden");
+  }
+});
+
+// go back to top when the scroll button is clicked
+scrollBtn.addEventListener("click", () => {
+  bodyElement.scrollIntoView();
+});
